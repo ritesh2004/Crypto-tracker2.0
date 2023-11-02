9b5a59d655d7e17a723fb8e052d68515dfbd166c
Ritesh Pramanik <pramanikritesh907@gmail.com>
2023-11-02 21:52:30 +0530

    .

--- app/components/Cards.jsx
+++ app/components/Cards.jsx
@@ -10,7 +10,6 @@ function Cards() {
     const items = coinArr?.filter((coin,id)=>(id<=5)).map((coin,id) => {
         return (
             <Card id={coin?.id} key={id} />
-            // <Card id={coin?.id} />
         )
     })
     // console.log(coinArr)
