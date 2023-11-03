951e63709d6225d8b400a3f1139f141dad27376a
Ritesh Pramanik <pramanikritesh907@gmail.com>
2023-11-03 10:42:32 +0530

    updated 3

--- app/api/NewsData.jsx
+++ app/api/NewsData.jsx
@@ -2,6 +2,7 @@ import axios from 'axios'
 import React from 'react'
 
 export const NewsData = async () => {
-  const {data} = await axios.get(`https://min-api.cryptocompare.com/data/v2/news/?lang=EN&api_key=cfe2e7fb0c0ff293b4019ad888e7626bdc860f730eb1e3e7a9c9c882f823057c`);
-  return data;
+  const resp = await axios.get(`https://min-api.cryptocompare.com/data/v2/news/?lang=EN&api_key=cfe2e7fb0c0ff293b4019ad888e7626bdc860f730eb1e3e7a9c9c882f823057c`);
+  console.log(resp)
+  return resp.data;
 }
